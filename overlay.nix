8b7869a63842c9562b35502394e162ec5e240ed7
kbwhodat <byantalok@yahoo.com>
2024-07-04 23:21:11 -0400

    adding tar

--- overlay.nix
+++ overlay.nix
@@ -3,19 +3,13 @@
 pkgs.stdenv.mkDerivation {
   pname = "undetected-chromedriver";
 
-  src = ./.;
   sourceRoot = ".";
 
   phases = [ "buildPhase" "installPhase" ];
-  nativeBuildInputs = [ pkgs.python311Packages.virtualenv pkgs.unzip ];
-
-  buildPhase = ''
-    virtualenv venv
-    source venv/bin/activate
-    pip install .
-  '';
+  buildInputs = [ pkgs.tar ];
 
   installPhase = ''
+    tar -xf undetected-chromedriver.tar.gz
     mkdir -p $out/bin
     mv undetected-chromedriver $out/bin/
     chmod +x $out/bin/undetected-chromedriver
