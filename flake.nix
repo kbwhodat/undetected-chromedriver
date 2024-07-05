88fabdecd6b9f463c1c25e3a3d886415f007a408
kbwhodat <byantalok@yahoo.com>
2024-07-05 06:58:19 -0400

    lock

--- flake.nix
+++ flake.nix
@@ -1,3 +1,10 @@
 {
-  outputs = { self,  ... }: { overlay = import ./overlay.nix; };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    {
+      overlays.default = final: prev: import ./overlay.nix { inherit final prev; };
+    };
 }
