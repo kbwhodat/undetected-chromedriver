1c50ff3671b839d118823f72d9e5f7d02c5c5756
kbwhodat <byantalok@yahoo.com>
2024-07-05 07:51:21 -0400

    simplate

--- flake.nix
+++ flake.nix
@@ -1,15 +1,4 @@
 {
-  description = "A simple example package for undetected-chromedriver";
-
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-  };
-
-  outputs = { self, nixpkgs, ... }:
-    {
-      overlays = {
-        default = final: prev: import ./overlay.nix { inherit final prev; };
-      };
-
-    };
+  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05"; };
+  outputs = { self, nixpkgs, ... }@inputs: { overlay = import ./overlay.nix; };
 }
